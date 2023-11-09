24ae2fb7d5ab16e5512fb04eeb364ae4360744b5
oxana-sh <121@jazz.su>
2023-11-09 09:43:03 +0300

    Add stroke

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
-  res.status(200).send('ok');
+  res.status(200).send('ok')
 })
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
