c6d13cafcd58ebb914f0a9bead3f870681507bb8
oxana-sh <121@jazz.su>
2023-11-10 07:05:15 +0300

    Add stroke

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Порт задается переменной окружения PORT, если она установлена, или 5000 по умолчанию
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.get('/health', (req, res) => {
   res.status(200).send('ok')
